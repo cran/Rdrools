b72d5baec85c204242a250a3c5f7f00d2e7fa26f
Ashwin Raaghav <Ashwin.Raaghav@mu-sigma.com>
2017-07-21 21:46:41 +0000

    version 1.0.2

--- R/Rdrools.R
+++ R/Rdrools.R
@@ -20,9 +20,9 @@
 }
 
 rulesSession<-function(rules,input.columns, output.columns) {
-	rules <- paste(rules, collapse="\n")
-	input.columns <- paste(input.columns,collapse=",")
-	output.columns <- paste(output.columns,collapse=",")
+	rules <- paste(rules, collapse='\n')
+	input.columns <- paste(input.columns,collapse=',')
+	output.columns <- paste(output.columns,collapse=',')
 	droolsSession<-.jnew('org/math/r/drools/DroolsService',rules,input.columns, output.columns)
 	return(droolsSession)
 }
@@ -31,9 +31,9 @@ runRules<-function(rules.session,input.df) {
 	conn<-textConnection('input.csv.string','w')
 	write.csv(input.df,file=conn)
 	close(conn)
-	input.csv.string <- paste(input.csv.string, collapse="\n")
-	output.csv.string <- .jcall(rules.session, "S", "execute",input.csv.string)
-	conn <- textConnection(output.csv.string, "r")
+	input.csv.string <- paste(input.csv.string, collapse='\n')
+	output.csv.string <- .jcall(rules.session, 'S', 'execute',input.csv.string)
+	conn <- textConnection(output.csv.string, 'r')
 	output.df<-read.csv(file=conn, header=T)
 	close(conn)
 	return(output.df)
