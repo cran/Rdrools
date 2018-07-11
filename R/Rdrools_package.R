318b8d9504b01a1059e1c666e942e6f0ef728e50
Naren Srinivasan <Naren.Srinivasan@mu-sigma.com>
2018-07-11 09:50:04 +0000

    version 1.1.0

---
+++ R/Rdrools_package.R
@@ -0,0 +1,13 @@
+#' Rdrools
+#' 
+#'  An interface for using the popular Java based Drools, which is a Business Rule Management System (See <https://www.drools.org> for more information). 
+#'  This package provides data scientists an intuitive interface to execute business rules on datasets for the purpose of analysis or designing intelligent systems, while leveraging the Drools rule engine. 
+#'  Rules can be defined in a simple format in a CSV, imported into the R Session as a data frame, and passed directly to the rule-checking functions.
+#'  Rules written in DRL format, accepted natively by Drools, can also be executed through an R interface. Credits to Mu Sigma for their continued support throughout the development of the package.
+#' 
+#' @docType package
+#' @name Rdrools
+#' @import rJava
+#' @import Rdroolsjars
+#' @import tidyverse
+NULL
